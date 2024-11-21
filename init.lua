15d60ede43abffc19f5966cfd04786cab20aea06
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2024-11-21 00:50:23 +0000

    NeoTree

--- init.lua
+++ init.lua
@@ -215,6 +215,16 @@ require('lazy').setup({
       end)
     end,
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function() end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
