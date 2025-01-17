f0f566b4098a4fab21b93d6307c80c819052f42c
Eduardo Barrancos <eduardobarrancos@premium-minds.com>
2025-01-17 09:22:56 +0000

    Updates

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -2,5 +2,5 @@ require('swagger-preview').setup {
   port = 8000,
   host = 'localhost',
 }
-vim.keymap.set('n', '<leader>gp', ':SwaggerPreview<CR>', { desc = 'swa[G]ger [P]review' })
-vim.keymap.set('n', '<leader>gr', ':SwaggerPreviewStop<CR> <BAR> :SwaggerPreview<CR>', { desc = 'swa[G]ger [R]estart' })
+vim.keymap.set('n', '<leader>wp', ':SwaggerPreview<CR>', { desc = 's[W]agger [P]review' })
+vim.keymap.set('n', '<leader>wr', ':SwaggerPreviewStop<CR> <BAR> :SwaggerPreview<CR>', { desc = 's[W]agger [R]estart' })
--- init.lua
+++ init.lua
@@ -108,6 +108,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>gu', function()
+  local uuid = vim.fn.system('uuidgen'):gsub('\n', '')
+  vim.api.nvim_put({ uuid }, 'c', true, true)
+end, { desc = '[G]enerate [U]uid' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
