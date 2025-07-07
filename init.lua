dcf357444c18d1447d4108efe313549590b60419
Eduardo Barrancos <eduardobarrancos@premium-minds.com>
2025-07-07 08:41:52 +0100

    Move up and down

--- init.lua
+++ init.lua
@@ -111,6 +111,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>mk', 'dd 3k p', { desc = '[M]ove line [K]up' })
+vim.keymap.set('n', '<leader>mj', 'dd p', { desc = '[M]ove line [J]down' })
+
 vim.keymap.set('n', '<leader>gu', function()
   local uuid = vim.fn.system('uuidgen'):gsub('\n', '')
   vim.api.nvim_put({ uuid }, 'c', true, true)
