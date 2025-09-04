5d8f5efe22fbbc0f6bf326e25a11d0a78a6bf826
Eduardo Barrancos <eduardobarrancos@premium-minds.com>
2025-09-04 11:03:44 +0100

    Burro. Este comando ja existe

--- init.lua
+++ init.lua
@@ -84,8 +84,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set('n', '<leader>gnd', vim.diagnostic.get_next, { desc = '[G]oto [N]ext [D]iagnostic' })
-
 vim.keymap.set('n', '<C_i>', 'i<C_t><Esc>', { desc = '[I]ndent the current line' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
